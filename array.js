1060416889339c8b15af8d348ab9209edadbbef4
Kauany Lima <kauanylima392@gmail.com>
2024-10-10 17:48:06 -0300

    Add files via upload

---
+++ array.js
@@ -0,0 +1,71 @@
+function exum (){ 
+let exercicioum = [1,2,3]
+exercicioum.push (4)
+console.log(exercicioum)
+}
+exum ()
+
+function exemplodois(){
+let exdois = [7,8,9]
+exdois.pop()
+console.log(exdois)
+}
+exemplodois ()
+
+function exemplotres (){
+let extres = ["manga","abacaxi"]
+extres.unshift("ameixa")
+console.log(extres)
+}
+exemplotres()
+
+function exemploquatro (){
+let exquatro = [4,5,6]
+exquatro.shift()
+console.log(exquatro)
+}
+exemploquatro()
+
+function exemplocinco (){
+let excinco = ["uva","abacate","laranja"]
+console.log(excinco)
+}
+exemplocinco()
+
+function exemploseis(){
+let exseis = []
+exseis.push(30)
+console.log(exseis)
+}
+exemploseis ()
+
+function exemplosete(){
+let exsete = ["banana"]
+exsete.push ("tangerina","maçã","limão")
+console.log(exsete)
+}
+exemplosete()
+
+function exemploOito (){
+let exoito = [6,7,8,9]
+exoito.pop()
+console.log(exoito)
+}
+exemploOito ()
+
+function exnove (){
+let exemplonove = [12,55,67] 
+if(exemplonove.length>5){
+    console.log("O Array tem mais que cinco elementos")
+}else{
+    console.log("O Array tem menos que cinco elementos")
+}
+}
+exnove()
+
+function exemplodez (){
+let exdezJabuticaba = ["mamão","coco"]
+exdezJabuticaba.push("morango")
+console.log(exdezJabuticaba)
+}
+exemplodez()
